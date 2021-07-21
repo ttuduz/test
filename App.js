b5a9eb783c189ce48f14d1c9d519bb82ad0ab4f8
kwklly <yeonlee@i.ewha.ac.kr>
2021-07-21 21:46:55 +0900

    initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+  
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Test1, Test2 } from './android/app/src/components'
+
+const Tab = createBottomTabNavigator();
+
+const TabNav = () => {
+  return (
+    <Tab.Navigator >
+      <Tab.Screen name="test1" component={Test1}/>
+      <Tab.Screen name="test2" component={Test2}/>
+    </Tab.Navigator>
+  )
+}
+
+export default TabNav;
